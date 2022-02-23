bb58cc469d693fa6d315d7fbff7bf05afe258163
Virusuk <70243256+Virusuki@users.noreply.github.com>
2022-02-23 09:46:52 +0900

    update

--- user/views.py
+++ user/views.py
@@ -14,7 +14,6 @@ def user(request):
         user = User.objects.all()
         return render(request, 'user/user_list.html', {'user_list':user})
         
-
     elif request.method == 'POST':
         data = JSONParser().parse(request)
         serializer = UserSerializer(data=data)
@@ -23,12 +22,13 @@ def user(request):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
+
 @csrf_exempt
 def login(request):
     if request.method == 'POST':
         name = request.POST['name']
-        print(User.objects.all().get(name=name))
+        User.objects.all().get(name=name)
         return render(request, 'user/success.html')
-    
+            
     elif request.method == 'GET':
         return render(request, 'user/login.html')
